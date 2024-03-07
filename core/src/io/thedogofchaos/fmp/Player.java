abb6f4145c1863b8fe1c4a9d429b4988e5015d46
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-03-07 12:17:15 +0000

    starting

---
+++ core/src/io/thedogofchaos/fmp/Player.java
@@ -0,0 +1,11 @@
+package io.thedogofchaos.fmp;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Player {
+    private Sprite playerSprite;
+
+    public Player(Sprite sprite) {
+        
+    }
+}
