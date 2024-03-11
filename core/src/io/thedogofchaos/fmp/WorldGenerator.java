64126a8aea1b63e561ef72b342baec1180f2fdc3
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-03-11 10:16:31 +0000

    sourced some things from the tests directory of the LibGDX repo
    https://github.com/libgdx/libgdx/blob/master/tests/gdx-tests/src/com/badlogic/gdx/tests/bench/TiledMapBench.java

--- core/src/io/thedogofchaos/fmp/WorldGenerator.java
+++ core/src/io/thedogofchaos/fmp/WorldGenerator.java
@@ -1,16 +1,25 @@
 package io.thedogofchaos.fmp;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayers;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class WorldGenerator {
-    private TiledMap map;
-    private TiledMapTileLayer mapLayer;
-    private TiledMapTileSet tileSet;
 
-    public GenerateWorld(int mapWidth, int mapHeight, int tileWidth, int tileHeight) {
-        map = loadMap();
-    }
 }
+
