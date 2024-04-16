f4a431f35ccabef19194a7456945d2743587c62e
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-04-16 14:45:07 +0100

    fuck

--- core/src/io/thedogofchaos/fmp/WorldGenerator.java
+++ core/src/io/thedogofchaos/fmp/WorldGenerator.java
@@ -1,70 +1,55 @@
 package io.thedogofchaos.fmp;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.MapLayers;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
-import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+
+import java.util.Arrays;
 
 public class WorldGenerator {
-    private static Texture tiles;
-    private static TiledMap map;
-
-    //TODO: make this use NoiseGenerator instead of randomly selecting tiles
-    public static TiledMap GenerateWorld(int mapWidth, int mapHeight, int tileWidth, int tileHeight, int exponent, int mapLayers, String noiseType) {
-        int x = 0;
-        int y = 0;
-        tiles = new Texture(Gdx.files.internal("spriteAtlas.png"));
-        TextureRegion[][] splitTiles = TextureRegion.split(tiles, tileWidth, tileHeight);
-        map = new TiledMap();
-        TiledMapTileLayer layer = new TiledMapTileLayer(mapWidth, mapHeight, tileWidth, tileHeight);
-        MapLayers layers = map.getLayers();
-        for (int i = 0; i<(mapWidth*mapHeight); i++) { // floor gen
-            Cell cell = new Cell();
-            cell.setTile(new StaticTiledMapTile(splitTiles[1][0]));
-            layer.setCell(x, y, cell);
+    // TODO: Rewrite this POS from scratch to make it work.
+    public static void GenerateWorld(int mapWidth, int mapHeight, int noiseExponent, int mapLayers, String noiseType) {
+        int[][][] mapArr = new int[mapLayers][mapWidth][mapHeight]; int x; int y; int l;
+        x=0;y=0;
+        for (int i = 0; i < (mapWidth * mapHeight); i++) {
+            mapArr[0][x][y] = 1;
             x++;
             if (x == mapWidth) {
                 y++;
                 x = 0;
             }
         }
-        layers.add(layer);
-        for (int l = 0; l < mapLayers; l++) { // wall gen
-            double[] perl;
+        for (l = 0; l < mapLayers; l++) {
+            x=0;y=0;
+            double[] noise;
             switch (noiseType) {
                 case "perlin":
-                    perl = NoiseGenerator.normalise(NoiseGenerator.perlinNoise(mapWidth, mapHeight, exponent));
+                    noise = NoiseGenerator.normalise(NoiseGenerator.perlinNoise(mapWidth, mapHeight, noiseExponent));
                     break;
                 case "smooth":
-                    perl = NoiseGenerator.normalise(NoiseGenerator.smoothNoise(mapWidth, mapHeight, exponent));
+                    noise = NoiseGenerator.normalise(NoiseGenerator.smoothNoise(mapWidth, mapHeight, noiseExponent));
                     break;
                 case "turbulence":
-                    perl = NoiseGenerator.normalise(NoiseGenerator.turbulence(mapWidth, mapHeight, exponent));
+                    noise = NoiseGenerator.normalise(NoiseGenerator.turbulence(mapWidth, mapHeight, noiseExponent));
                     break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + noiseType);
             }
-            x = 0;
-            y = 0;
-            for (double v : perl) {
-                Cell cell = new Cell();
+            for (double v : noise) {
                 if (Math.round(v) == 1) {
-                    cell.setTile(new StaticTiledMapTile(splitTiles[0][1]));
+                    mapArr[l][x][y] = 10;
+                    // TODO: Do this next
+                } else if (Math.round((v*2)+0.5)==0.5) {
+                    mapArr[l][x][y] = 9;
+                } else {
+                    mapArr[l][x][y] = 8;
                 }
-                layer.setCell(x, y, cell);
                 x++;
                 if (x == mapWidth) {
                     y++;
                     x = 0;
                 }
             }
-            layers.add(layer);
+            System.out.println(Arrays.deepToString(mapArr));
         }
-        return map;
     }
 }
-
