437809feda3d8edb2de470725a8479bcc144ed04
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-05-15 16:34:20 +0100

    This shit doesn't work.
    Might have to entirely switch to @Anuken's Arc framework with what I'm trying to do here.

--- core/src/io/thedogofchaos/fmp/fragment/BuildMenuFragment.java
+++ core/src/io/thedogofchaos/fmp/fragment/BuildMenuFragment.java
@@ -3,8 +3,12 @@ package io.thedogofchaos.fmp.fragment;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
@@ -22,12 +26,18 @@ public class BuildMenuFragment implements Disposable {
         buildMenuTable.bottom().padBottom(16);
     }
 
-    public static void show(){
-        for (int i = 0; i < Blocks.blockList.length; i++){
+    public static void show() {
+        for (int i = 0; i < Blocks.blockList.length; i++) {
             ImageButton blockButton = new ImageButton(new TextureRegionDrawable(Blocks.blockList[i].blockTextureRegion));
             blockButton.setTransform(true);
             blockButton.setOrigin(blockButton.getPrefWidth() / 2, blockButton.getPrefHeight() / 2);
-            blockButton.setScale(2f,2f);
+            blockButton.setScale(2f, 2f);
+            blockButton.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    Gdx.app.log("INFO", "balls");
+                }
+            });
             buildMenuTable.add(blockButton);
         }
     }
